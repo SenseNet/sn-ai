f95d9966182c1a3c47d4177a2b864843f1d6b042
tusmester <tusmester@gmail.com>
2024-01-05 16:24:34 +0100

    AI Vision packages initial commit.

---
+++ src/SenseNet.AI.Vision.Azure/AzureVisionOptions.cs
@@ -0,0 +1,28 @@
+﻿using SenseNet.Tools.Configuration;
+
+namespace SenseNet.AI.Vision.Azure;
+
+/// <summary>
+/// Options for the Azure Vision feature.
+/// </summary>
+[OptionsClass(sectionName: "sensenet:ai:AzureVision")]
+public class AzureVisionOptions
+{
+    /// <summary>
+    /// The model to use for image generation. Default: "dall-e-3".
+    /// </summary>
+    public string Model { get; set; } = "dall-e-3";
+    /// <summary>
+    /// The Azure endpoint to use. If not set, the default OpenAI endpoint will be used.
+    /// </summary>
+    public string? AzureEndpoint { get; set; }
+    
+    /// <summary>
+    /// OpenAI API key
+    /// </summary>
+    public string? OpenAiApiKey { get; set; }
+    /// <summary>
+    /// OpenAI organization ID
+    /// </summary>
+    public string? OrgId { get; set; }
+}
---
+++ src/SenseNet.AI.Vision/IImageGenerator.cs
@@ -0,0 +1,25 @@
+﻿namespace SenseNet.AI.Vision;
+
+/// <summary>
+/// Defines methods for generating images from text.
+/// </summary>
+public interface IImageGenerator
+{
+    /// <summary>
+    /// Generates an image from the given text.
+    /// </summary>
+    /// <param name="text">The text to generate the image from.</param>
+    /// <param name="width">The width of the image.</param>
+    /// <param name="height">The height of the image.</param>
+    /// <param name="cancel">The cancellation token.</param>
+    /// <remarks>
+    /// The maximum length of the text is 1000 characters in case of the 
+    /// dall-e-2 engine and 4000 characters in case of dall-e-3.
+    /// The size of the generated image depends on the provided width and height.
+    /// The engine will choose one of the available sizes that fits the
+    /// provided dimensions. The available sizes are: '256x256', '512x512', 
+    /// '1024x1024', '1024x1792', '1792x1024'.
+    /// </remarks>
+    /// <returns>The image data of the generated image.</returns>
+    Task<ImageData> GenerateImage(string text, int width, int height, CancellationToken cancel);
+}
