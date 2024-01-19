618a27c126c8a56d11e3ea4fb2f2cb7850096f0b
Miklós Tóth <tusmester@gmail.com>
2024-01-19 19:57:50 +0100

    Add plugin API to the semantic kernel project. (#13)
    * Add plugin API to the semantic kernel project.
    
    * Let plugins use the DI service provider.

---
+++ src/SenseNet.AI.CLI/SenseNetKernelPlugin.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using Microsoft.SemanticKernel;
+using SenseNet.Client;
+
+namespace SenseNet.AI.Text.SemanticKernel;
+
+/// <summary>
+/// Sample plugin for the semantic kernel.
+/// </summary>
+public sealed class SenseNetKernelPlugin
+{
+    private readonly IRepositoryCollection _repositories;
+
+    public SenseNetKernelPlugin(IRepositoryCollection repositories)
+    {
+        _repositories = repositories;
+    }
+    
+    [KernelFunction, Description("Determines the path of a folder based on its name")]
+    public async Task<string> GetContainerPath(
+        [Description("The name of the folder")] string name, CancellationToken cancel)
+    {
+        var repo = await _repositories.GetRepositoryAsync(cancel);
+        var folders = await repo.QueryAsync(new QueryContentRequest
+        {
+            ContentQuery = $"TypeIs:Folder AND Name:\"{name}\"",
+            Select = new[] { "Id", "Path", "Type" }
+        }, cancel);
+
+        var paths = string.Join(", ", folders.Select(f => "\"" + f.Path + "\""));
+        var pathResult = $"{{ \"Path\": [ {paths} ] }}";
+        
+        return pathResult;
+    }
+
+    [KernelFunction, Description("Determines the id of a user based on its name, login name or username")]
+    public Task<string> GetUserId(
+        [Description("The name of the user")] string name)
+    {
+        return Task.FromResult("123");
+    }    
+}
--- src/SenseNet.AI.Text.SemanticKernel/ContentQueryGenerator.cs
+++ src/SenseNet.AI.Text.SemanticKernel/ContentQueryGenerator.cs
@@ -15,6 +15,7 @@ public class ContentQueryGenerator : IContentQueryGenerator, ISnFeature
 {
     private readonly SemanticKernelOptions _options;
     private readonly ILogger<ContentQueryGenerator> _logger;
+    private readonly IServiceProvider _serviceProvider;
 
     #region ISnFeature implementation    
 
@@ -33,10 +34,11 @@ public class ContentQueryGenerator : IContentQueryGenerator, ISnFeature
 
     #endregion
 
-    public ContentQueryGenerator(IOptions<SemanticKernelOptions> options, ILogger<ContentQueryGenerator> logger)
+    public ContentQueryGenerator(IOptions<SemanticKernelOptions> options, ILogger<ContentQueryGenerator> logger, IServiceProvider serviceProvider)
     {
         _options = options.Value;
         _logger = logger;
+        _serviceProvider = serviceProvider;
     }
 
     public Task<QueryData> GenerateQueryAsync(string text, CancellationToken cancel)
@@ -59,7 +61,16 @@ public class ContentQueryGenerator : IContentQueryGenerator, ISnFeature
         //TODO: can we use a singleton kernel builder, defined during startup in DI?
 
         var builder = await AssistantBuilder.GetAssistantAsync(_options.OpenAiApiKey, 
-            _options.AssistantId, cancellationToken: cancel);
+            _options.AssistantId,             
+            cancellationToken: cancel);
+
+        if (_options.ConfigureDefaultPlugins != null)
+        {            
+            _options.ConfigureDefaultPlugins(builder.Plugins, _serviceProvider);
+
+            _logger.LogTrace("Configured OpenAI Assistant query generator plugins: " +
+                $"{string.Join(", ", builder.Plugins.Select(p => p.Name))}");
+        }
 
         IChatThread? thread = null;
 
@@ -82,6 +93,11 @@ public class ContentQueryGenerator : IContentQueryGenerator, ISnFeature
             _logger.LogTrace("Creating new OpenAI Assistant thread");
             thread = await builder.NewThreadAsync(cancel);
         }
+
+        // add more context to the text
+        text = $"{text}" + Environment.NewLine + 
+            $"<currentdate>{DateTime.UtcNow.Date}</currentdate>" +
+            $"<currenttime>{DateTime.UtcNow}</currenttime>";
         
         await thread.AddUserMessageAsync(text, cancel);
         var messages = thread.InvokeAsync(builder, cancel);
